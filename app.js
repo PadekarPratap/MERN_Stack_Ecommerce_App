a621d419eb9b64ff0e503eeffa4effa6a827eadb
Pratap Padekar <unitedpratap7@gmail.com>
2023-07-07 16:04:40 +0530

    Minor Fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cors({origin: ['http://localhost:5173', process.env.CLIENT], credentials: true}))
+app.use(cors({origin: ['https://codeshoppro.netlify.app'], credentials: true}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
